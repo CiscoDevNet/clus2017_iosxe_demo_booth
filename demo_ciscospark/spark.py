a4938f441ea3b3282a571d234dda70c6060de7ae
Jeff McLaughlin <jeff@jeffmc.net>
2017-06-17 13:39:52 -0700

    Added on-box cfg diff script

---
+++ demo_ciscospark/spark.py
@@ -0,0 +1,119 @@
+import requests
+from requests.auth import HTTPBasicAuth
+from requests.packages.urllib3.exceptions import InsecureRequestWarning
+import json
+
+proxies = {
+	"http":"http://173.36.224.108:8080",
+	"https":"https://173.36.224.108:8080"
+}
+
+def create_room(room_name, token):
+	
+	requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
+
+	headers = {'Authorization':'Bearer '+token,
+				'Content-Type':'application/json'}
+	body = json.dumps({'title':room_name})
+
+	resp = requests.post('https://api.ciscospark.com/v1/rooms',
+	verify=False,headers=headers,data=body)
+
+	print resp
+
+def list_rooms(token):
+	
+	requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
+
+	headers = {'Authorization':'Bearer '+token,
+				'Content-Type':'application/json'}
+	resp = requests.get('https://api.ciscospark.com/v1/rooms',
+	verify=False,headers=headers)
+
+	return resp
+
+def get_room_id(room_name, token):
+
+	requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
+
+	id = ""
+
+	headers = {'Authorization':'Bearer '+token,
+				'Content-Type':'application/json'}
+	resp = requests.get('https://api.ciscospark.com/v1/rooms',
+	verify=False,headers=headers,proxies=proxies)
+
+	if resp.status_code == 200:
+		
+		rooms = json.loads(resp.text)['items']
+
+		for room in rooms:
+			if room['title'] == room_name:
+				id = room['id']
+
+	return id
+
+def list_messages(room_id, token):
+
+	requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
+
+	headers = {'Authorization':'Bearer '+token,
+				'Content-Type':'application/json'}
+	resp = requests.get('https://api.ciscospark.com/v1/messages?roomId={}'.format(room_id),
+						verify=False,headers=headers)
+	return resp.text
+
+def post_message(message_text, room_id, token):
+
+	requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
+
+	headers = {'Authorization':'Bearer '+token,
+				'Content-Type':'application/json'}
+
+	body = json.dumps({'roomId':room_id,'text':message_text})
+
+	resp = requests.post('https://api.ciscospark.com/v1/messages',
+	verify=False,headers=headers,data=body,proxies=proxies)
+
+	return resp
+
+def post_message_with_image(message_text, img_url, room_id, token):
+
+	requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
+
+	headers = {'Authorization':'Bearer '+token,
+				'Content-Type':'application/json'}
+
+	body = json.dumps({'roomId':room_id,'text':message_text, 'files':img_url})
+
+	resp = requests.post('https://api.ciscospark.com/v1/messages',
+	verify=False,headers=headers,data=body)
+
+	return resp
+
+def cleanup_room(room_id, token):
+
+	requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
+
+	messages = json.loads(list_messages(room_id, token))
+
+	headers = {'Authorization':'Bearer '+token,
+				'Content-Type':'application/json'}
+
+	for message in messages['items']:
+
+		resp = requests.delete('https://api.ciscospark.com/v1/messages/{}'.format(message['id']),
+					verify=False,headers=headers)
+		print resp.status_code
+
+
+
+def main():
+
+	room=get_room_id('jeff test room')
+	print post_message('Testing 6:32pm',room).status_code
+	list_messages(room)
+
+if __name__ == "__main__":
+
+	main()
---
+++ demo_ciscospark/sparkcfg.py
@@ -0,0 +1,83 @@
+import os
+import sys
+from cli import cli
+import time
+import difflib
+from spark import *
+
+SPARK_ROOM = "Catalyst OnBox"
+
+my_token = "ODY1ZWE3ZTUtY2JhNS00Njc2LWI1MDYtMTAwY2EwM2FkNzFmYTM4YzdhMjgtODI5"
+bot_token = "Mjg3ZWVjMTYtYzc2NC00ZDc2LTg5ZWMtYjJlMmYyYTllZjA5YzlhM2ZhYmMtZTAy"
+
+def save_config():
+
+	output = cli('show run')
+
+	timestr = time.strftime("%Y%m%d-%H%M%S")
+	filename = "/configs/" + timestr + "_shrun"
+
+	f = open(filename,"w")
+	f.write(output)
+	f.close
+
+	f = open('/configs/current_config_name','w')
+	f.write(filename)
+	f.close
+
+	return filename
+
+def get_cfg_fn():
+
+	try:
+		f = open('/configs/current_config_name','r')
+	except:
+		return None
+	fn = f.read()
+	f.close()
+	return fn
+
+def compare_configs(cfg1,cfg2):
+
+	d = difflib.unified_diff(cfg1, cfg2)
+
+	diffstr = ""
+
+	for line in d:
+		if line.find('Current configuration') == -1:
+			if line.find('Last configuration change') == -1:
+				if (line.find("+++")==-1) and (line.find("---")==-1):
+					if (line.find("-!")==-1) and (line.find('+!')==-1):
+						if line.startswith('+'):
+							diffstr = diffstr + "\n" + line
+						elif line.startswith('-'):
+							diffstr = diffstr + "\n" + line
+
+	return diffstr
+
+if __name__ == '__main__':
+
+	old_cfg_fn = get_cfg_fn()
+
+	if not old_cfg_fn:
+		#  First time we are running
+		save_config()
+		sys.exit()
+
+	new_cfg_fn = save_config()
+
+	f = open(old_cfg_fn)
+	old_cfg = f.readlines()
+	f.close
+
+	f = open(new_cfg_fn)
+	new_cfg = f.readlines()
+	f.close
+
+	os.remove(old_cfg_fn)
+
+	text = "Configuration change detected:" + "\n" + compare_configs(old_cfg,new_cfg)
+
+	room = get_room_id(SPARK_ROOM, my_token)
+
+	post_message(text, room, bot_token)
